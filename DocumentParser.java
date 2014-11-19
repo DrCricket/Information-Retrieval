5e8d17e1c86e3514358bb31a253ddeb7352d0e32
DrCricket <utsav.tiwary2011@gmail.com>
2014-11-19 22:48:50 +0530

    Information Retrieval using Fuzzy Logic

--- DocumentParser.java
+++ DocumentParser.java
@@ -1,12 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.*/
 
-/**
- *
- * @author Dhruv
- */
 import java.util.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -15,9 +7,9 @@ public class DocumentParser {
     ArrayList docs;
     ArrayList terms;
     Document d;
-    HashMap<String,Integer> docFrequency; 
+    HashMap<String,Integer> docFrequency;
     HashMap<String,Integer> stopList;
-    
+
     DocumentParser(String dir){
         docs=new ArrayList<Document>();
         stopList=new HashMap<String,Integer>();
@@ -26,7 +18,7 @@ public class DocumentParser {
         parse(dir);
         //System.out.println(docFrequency.toString());
     }
-    
+
     void handleStopwords(String dir){
         BufferedReader br=readFile(dir,"stoplist.txt");
         String currLine;
@@ -39,7 +31,7 @@ public class DocumentParser {
             System.out.println("problem with stoplist");
         }
     }
-    
+
     void parse(String Dir){
         BufferedReader br=readFile(Dir,"fileNames.txt");
         String fileToRead;
@@ -57,31 +49,31 @@ public class DocumentParser {
                     text.append(dummy);
                 }
                 //System.out.println("text is  "+text);
-                
-                
-               StringTokenizer st=new StringTokenizer(text.toString()," ,-."); 
+
+
+               StringTokenizer st=new StringTokenizer(text.toString()," ,-.");
                String t;
                while(st.hasMoreTokens()){
             	   t=st.nextToken();
             	   if(stopList.get(t)==null){
                        //System.out.println("word to add = "+words[i]);
                        addWord(d,t.toLowerCase());
-                   
+
                    }
                }
-                
-                
+
+
                 //System.out.println(d.h.toString());
                 docs.add(d);
             }
         }catch(Exception e){
             System.out.println("exception caught in reading the file");
         }
-        
+
     }
-    
-    
-    
+
+
+
     BufferedReader readFile(String Dir,String file){
          BufferedReader br=null;
          try{
@@ -91,9 +83,9 @@ public class DocumentParser {
          }
         return br;
     }
-    
-    
-    
+
+
+
     void addWord(Document d,String word){
         d.numberOfWords+=1;
         //System.out.println("reached addWord with word = "+word);
@@ -109,10 +101,10 @@ public class DocumentParser {
         }
         //System.out.println("added word ");
     }
-    
+
     void parseQuery(String query){
         //we calculate tf-idf here.
-        //double nested loop 
+        //double nested loop
         terms=new ArrayList();
         String words[]=query.split(" ");
         int i=0;
@@ -135,7 +127,7 @@ public class DocumentParser {
         double tf,idf,dmtf;
         double maxtfidf,maxBM25;
         maxtfidf=0;
-        maxBM25=0;    
+        maxBM25=0;
         while(docItr.hasNext()){
             d=(Document)docItr.next();
             itr=terms.iterator();
@@ -144,39 +136,39 @@ public class DocumentParser {
                 term=(String)itr.next();
                 //if(d.h.get(term)!=null&&docFrequency.get(term)!=null){
                 double val1,val2;
-                
+
                 if(d.h.get(term)==null)
                 	val1=0;
-                else 
+                else
                 	val1=d.h.get(term);
-                
-                
+
+
                 if(docFrequency.get(term)==null)
                 	val2=0;
-                else 
+                else
                 	val2=docFrequency.get(term);
-                
+
                     tf=1+Math.log(1.01+val1);
-                    
-                    
+
+
                     idf=1+(double)((docs.size()-val2+0.5)/(val2+0.5));
                     idf=Math.log(idf);
                     //System.out.println("file : "+d.docName+"  term : "+term+ ". term frquency "+val1+". doc frequency : "+val2+". tf value : "+tf+" idf value : "+idf);
                     d.Rank+=tf*idf;
-                    
+
                     dmtf=(val1*(1.5+1))/(val1+1.5*(0.25+0.25*(d.numberOfWords/avgdl)));
                     d.RankBM25+=idf*dmtf;
                 //}
-                
+
             }
-            
+
             if(d.Rank>maxtfidf)
                 maxtfidf=d.Rank;
             if(d.RankBM25>maxBM25)
                 maxBM25=d.RankBM25;
-            
+
         }
-        
+
         docItr=docs.iterator();
         while(docItr.hasNext()){
             d=(Document)docItr.next();
@@ -184,15 +176,15 @@ public class DocumentParser {
             	//d.Rank=0;
             	d.RankBM25=0;
             }else {
-            	
+
                 d.RankBM25=d.RankBM25/maxBM25;
             }
             d.Rank=d.Rank/maxtfidf;
-            
-            
+
+
             //System.out.println("tfidf rank is : "+d.Rank+". bm25 rank is : "+d.RankBM25);
         }
-        
-        
+
+
     }
 }
